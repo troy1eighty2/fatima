fdfa1da2f98022505b5f90e266c0235ee3fdcd6e
troy1eighty2 <troytran000@gmail.com>
2024-11-04 18:09:39 -0600

    footerleft

--- src/components/FooterLeft.jsx
+++ src/components/FooterLeft.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+import styles from "./FooterLeft.module.css"
 function FooterLeft() {
   return <>
+    <div className={styles.container}>
+      <div className={styles.item1}><Link to="/">Home</Link></div>
+      <div className={styles.item2}><Link to="/contact">Get A Quote</Link></div>
+      <div className={styles.item3}><a href="mailto:troytran000@gmail.com" target="">Email</a></div>
+      <div className={styles.item4}><a href="https://www.instagram.com/jonydoce/" target="_blank">Instagram</a></div>
+      <div className={styles.item5}><Link to="/faq">FAQ's</Link></div>
+    </div>
   </>
 }
 
