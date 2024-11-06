b0a91eaa3bb83b2d91115dcf884fa3782c76fd98
troy1eighty2 <troytran000@gmail.com>
2024-11-06 15:07:18 -0600

    faq

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,9 +1,11 @@
 import styles from "./Faq.module.css"
+import Question from "../parts/Question.jsx"
 function Faq() {
   return <>
     {/* database for questions */}
     <div className={styles.container}>
-      <h1>Frequently Asked Questions</h1>
+      <h1 className={styles.header}>Frequently Asked Questions</h1>
+      <Question></Question>
     </div>
   </>
 }
---
+++ src/parts/Question.jsx
@@ -0,0 +1,35 @@
+import styles from "./Question.module.css"
+function Question({ question, answer }) {
+  return <>
+    <div className={styles.container}>
+      <h2 className={styles.header}>How this</h2>
+      <p>do this</p>
+    </div>
+    <div className={styles.container}>
+      <h2 className={styles.header}>How this</h2>
+      <p>do this</p>
+    </div>
+    <div className={styles.container}>
+      <h2 className={styles.header}>How this</h2>
+      <p>do this</p>
+    </div>
+    <div className={styles.container}>
+      <h2 className={styles.header}>How this</h2>
+      <p>do this</p>
+    </div>
+    <div className={styles.container}>
+      <h2 className={styles.header}>How this</h2>
+      <p>do this</p>
+    </div>
+    <div className={styles.container}>
+      <h2 className={styles.header}>How this</h2>
+      <p>do this</p>
+    </div>
+    <div className={styles.container}>
+      <h2 className={styles.header}>How this</h2>
+      <p>do this</p>
+    </div>
+  </>
+}
+
+export default Question;
