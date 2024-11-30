13f85bf46bd714ecc4378167594aebd772b39dc0
troy1eighty2 <troytran000@gmail.com>
2024-11-30 11:22:50 -0600

    shop

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,22 @@
+import styles from "./Shop.module.css";
+import { useState } from "react";
+import Store from "../parts/Store.jsx";
+import jawn from "../assets/Assets/Assets/temp/jawn.png";
+import jawn1 from "../assets/Assets/Assets/temp/jawn1.png";
+import wrestler from "../assets/Assets/Assets/Deliverables/Illustrations/wwe.png"
+const items = [
+  jawn,
+  jawn1,
+  jawn,
+  jawn1
+]
 function Shop() {
+  const [loading, setLoading] = useState(true)
+
   return <>
-    Shop
+    <div className={styles.container}>
+      {items.length == 0 || items.length == null ? <img src={wrestler} className={styles.loadingimg} /> : <Store products={items}></Store>}
+    </div>
   </>
 }
 
---
+++ src/parts/Store.jsx
@@ -0,0 +1,15 @@
+import styles from "./Store.module.css";
+import { useState } from "react";
+
+function Store({ products }) {
+  return <>
+    <div className={styles.container}>
+      <p className={styles.header}>All Products</p>
+      <div className={styles.products}>
+        {products.map((item, index) => <img src={item} className={styles.product} key={index}></img>)}
+      </div>
+    </div>
+  </>
+
+}
+export default Store;
