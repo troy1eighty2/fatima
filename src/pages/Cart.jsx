4cc7afbdbf2ead619945602d6b9a871cf89d5c36
troy1eighty2 <troytran000@gmail.com>
2025-01-15 20:45:38 -0600

    product work

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,7 +2,8 @@ import styles from "./Cart.module.css"
 import FooterRight from "../components/FooterRight";
 import CartEmpty from "../parts/CartEmpty.jsx"
 import CartFull from "../parts/CartFull.jsx"
-import testdata from "../cart.json"
+import testdata from "../cart.json";
+import { v4 as uuidv4 } from 'uuid';
 function Cart() {
   return <>
     <div className={styles.container}>
--- src/parts/SizeSelection.jsx
+++ src/parts/SizeSelection.jsx
@@ -1,17 +1,21 @@
 import styles from "./SizeSelection.module.css"
+import { useState } from "react";
 function SizeSelection() {
   const sizes = ["XS", "S", "M", "L", "XL", "XXL"]
   const colors = ["#E46635", "#C58EB6", "#F86381"]
+  const [selected, setSelected] = useState(null);
   return <>
 
     <div className={styles.container}>
       {sizes.map((size, index) => (
-        <button key={index} className={styles.size}>
+        <button key={index} className={styles.size} style={selected === size ? { backgroundColor: colors[index % colors.length] } : { backgroundColor: "unset" }} onClick={() => {
+          setSelected(size)
+        }}>
           <p>{size}</p>
 
         </button>
       ))}
-    </div>
+    </div >
   </>
 
 }
